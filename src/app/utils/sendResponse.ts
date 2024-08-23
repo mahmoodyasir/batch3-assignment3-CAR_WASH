aff0a8eefafd24c59db9feb8ac337a515337ca15
mahmoodyasir <yasirmamun16@gmail.com>
2024-08-23 16:05:31 +0600

    No data Found response done

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -4,11 +4,27 @@ type TResponse<T> = {
   statusCode: number;
   success: boolean;
   message?: string;
-  token?:string;
+  token?: string;
   data: T;
 };
 
 const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+
+  const isEmptyData = !data?.data ||
+    (Array.isArray(data?.data) && data?.data?.length === 0) ||
+    (typeof data?.data === 'object' && Object.keys(data?.data)?.length === 0);
+
+
+  if (isEmptyData) {
+    return res.status(404).json({
+      success: false,
+      statusCode: 404,
+      message: 'No Data Found',
+      data: []
+    });
+  }
+
+
   res.status(data?.statusCode).json({
     success: data.success,
     statusCode: data?.statusCode,
